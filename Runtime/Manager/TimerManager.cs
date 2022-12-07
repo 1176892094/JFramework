64388a9b95f1ed0191e2b329447a33822c6ab5fe
jinyijie <1176892094@qq.com>
2022-12-08 01:14:11 +0800

    3
    3

--- Runtime/Manager/TimerManager.cs
+++ Runtime/Manager/TimerManager.cs
@@ -1,6 +1,5 @@
-using System;
 using System.Collections.Generic;
-using UnityEngine;
+using JFramework.Basic;
 
 namespace JFramework
 {
@@ -33,75 +32,5 @@ namespace JFramework
         }
 
         public void Clear() => timerList.Clear();
-    }
-    
-    public class Timer
-    {
-        private int curLoop;
-        private int maxLoop;
-        private bool isStop;
-        private bool isRunning;
-        private float curTime;
-        private float stopTime;
-        private float delayTime;
-        private Action OnFinished;
-
-        public void Open(float delayTime, Action OnFinished = null)
-        {
-            maxLoop = 1;
-            this.delayTime = delayTime;
-            this.OnFinished = OnFinished;
-            Start();
-        }
-
-        public void Open(float delayTime, int maxLoop, Action OnFinished = null)
-        {
-            this.maxLoop = maxLoop;
-            this.delayTime = delayTime;
-            this.OnFinished = OnFinished;
-            Start();
-        }
-
-        private void Start()
-        {
-            curLoop = -1;
-            isRunning = true;
-            curTime = Time.time;
-            TimerManager.Instance.AddTimer(this);
-        }
-
-        public void Update()
-        {
-            if (!isRunning || Time.time <= curTime) return;
-            curTime += delayTime;
-            curLoop++;
-            if (curLoop == 0) return;
-            OnFinished.Invoke();
-            if (curLoop <= maxLoop) return;
-            Close();
-        }
-
-        public void Stop()
-        {
-            if (isStop) return;
-            isStop = true;
-            isRunning = false;
-            stopTime = Time.time;
-        }
-
-        public void Play()
-        {
-            if (!isStop) return;
-            isStop = false;
-            isRunning = true;
-            curTime += Time.time - stopTime;
-        }
-
-        public void Close()
-        {
-            TimerManager.Instance.RemoveTimer(this);
-            isRunning = false;
-            OnFinished = null;
-        }
     }
 }
