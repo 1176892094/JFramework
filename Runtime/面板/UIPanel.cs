874d86f92f68e382f3385a56f7dd453d68b5cf92
jinyijie <1176892094@qq.com>
2023-03-19 18:36:14 +0800

    Update UIPanel.cs

--- Runtime/面板/UIPanel.cs
+++ Runtime/面板/UIPanel.cs
@@ -79,7 +79,7 @@ namespace JFramework
         /// <param name="key">组件的名称</param>
         /// <typeparam name="T">可以使用任何继承UIBehaviour的组件</typeparam>
         /// <returns>返回查找到的组件</returns>
-        protected T Get<T>(string key) where T : UIBehaviour
+        public T Get<T>(string key) where T : UIBehaviour
         {
             var type = typeof(T).Name;
             if (containerDict.ContainsKey(type))
