91199a7c304882bee27bb8e542551bf36e5435ff
jinyijie <1176892094@qq.com>
2023-03-21 19:20:26 +0800

    Update Entity.cs

--- Runtime/实体/Entity.cs
+++ Runtime/实体/Entity.cs
@@ -14,7 +14,9 @@ namespace JFramework
     {
         private Dictionary<string, IController> controlDict;
 
-        protected virtual void Spawn(params object[] value) => controlDict = new Dictionary<string, IController>();
+        protected virtual void Spawn(params object[] value)
+        {
+        }
 
         protected virtual void Enable()
         {
@@ -45,6 +47,7 @@ namespace JFramework
         public T Get<T>() where T : ScriptableObject, IController
         {
             var key = typeof(T).Name;
+            controlDict ??= new Dictionary<string, IController>();
             if (controlDict.ContainsKey(key)) return (T)controlDict[key];
             var controller = ScriptableObject.CreateInstance<T>();
             controlDict.Add(key, controller);
