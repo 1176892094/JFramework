b436239cfe0efa5e5c433e60d5c8eeb305e3b437
Charlotte <1176892094@qq.com>
2023-07-28 21:12:04 +0800

    Update UIStateType.cs

--- Runtime/面板/UIStateType.cs
+++ Runtime/面板/UIStateType.cs
@@ -4,18 +4,21 @@ namespace JFramework
     {
         /// <summary>
         /// HidePanel => 移除并销毁
+        /// panel.Hide => 执行
         /// Clear => 移除并销毁
         /// </summary>
         Common = 0,
 
         /// <summary>
         /// HidePanel => 不移除不销毁
+        /// panel.Hide => 不执行 (按钮无法响应)
         /// Clear => 移除并销毁
         /// </summary>
         Freeze = 1,
 
         /// <summary>
         /// HidePanel => 不移除不销毁
+        /// panel.Hide => 执行
         /// Clear => 不移除不销毁
         /// </summary>
         Ignore = 2,
