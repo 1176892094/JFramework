397a09dedc292a82ca9979fb184486b5dd18ba80
Charlotte <1176892094@qq.com>
2023-07-29 20:36:16 +0800

    Update StringExtensions.cs

--- Runtime/拓展/StringExtensions.cs
+++ Runtime/拓展/StringExtensions.cs
@@ -1,46 +1,17 @@
+
 // ReSharper disable All
 namespace JFramework
 {
     public static partial class Extensions
     {
         public static bool IsEmpty(this string s) => string.IsNullOrEmpty(s);
-        public static string Green(this string s) => s.SetColor(TextColor.Green);
-        public static string White(this string s) => s.SetColor(TextColor.White);
-        public static string Purple(this string s) => s.SetColor(TextColor.Purple);
-        public static string Yellow(this string s) => s.SetColor(TextColor.Yellow);
-        public static string Orange(this string s) => s.SetColor(TextColor.Orange);
-        public static string Pink(this string s) => s.SetColor(TextColor.Pink);
-        public static string Blue(this string s) => s.SetColor(TextColor.Blue);
-        public static string Red(this string s) => s.SetColor(TextColor.Red);
-        public static string Sky(this string s) => s.SetColor(TextColor.Sky);
-        internal static string SetColor(this string s, TextColor type)
-        {
-            return type switch
-            {
-                TextColor.White => "<color=#FFFFFF>" + s + "</color>",
-                TextColor.Yellow => "<color=#FFFF00>" + s + "</color>",
-                TextColor.Sky => "<color=#00FFFF>" + s + "</color>",
-                TextColor.Purple => "<color=#FF00AA>" + s + "</color>",
-                TextColor.Orange => "<color=#FFAA00>" + s + "</color>",
-                TextColor.Red => "<color=#FF0000>" + s + "</color>",
-                TextColor.Blue => "<color=#00CCFF>" + s + "</color>",
-                TextColor.Green => "<color=#00FF00>" + s + "</color>",
-                TextColor.Pink => "<color=#FFAACC>" + s + "</color>",
-                _ => s
-            };
-        }
-    }
-
-    internal enum TextColor
-    {
-        Sky = 0,
-        Red = 1,
-        Blue = 2,
-        Pink = 3,
-        Green = 4,
-        White = 5,
-        Yellow = 6,
-        Purple = 7,
-        Orange = 8,
+        public static string Green(this string s) => s.Color(0x00FF00);
+        public static string Yellow(this string s) => s.Color(0xFFFF00);
+        public static string Orange(this string s) => s.Color(0xFFAA00);
+        public static string Pink(this string s) => s.Color(0xFFAACC);
+        public static string Blue(this string s) => s.Color(0x00CCFF);
+        public static string Red(this string s) => s.Color(0xFF0000);
+        public static string Sky(this string s) => s.Color(0x00FFFF);
+        public static string Color(this string s, int color) => $"<color=#{color:X}>{s}</color>";
     }
 }
