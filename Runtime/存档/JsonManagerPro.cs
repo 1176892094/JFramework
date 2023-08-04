6900bd44c9a7c8330657c567ef5cc61fe3b99f59
Charlotte <1176892094@qq.com>
2023-08-04 19:27:56 +0800

    Update JsonManagerPro.cs

--- Runtime/存档/JsonManagerPro.cs
+++ Runtime/存档/JsonManagerPro.cs
@@ -32,12 +32,13 @@ namespace JFramework.Core
         public static async Task Decrypt(ScriptableObject obj)
         {
             var filePath = GetPath(obj.name);
+            secrets ??= await Load<Dictionary<string, JsonData>>(nameof(JsonManager));
             if (!File.Exists(filePath))
             {
                 Debug.Log($"{nameof(JsonManager)} 创建 => {obj.name.Orange()} 数据文件");
                 await Encrypt(obj, obj.name);
             }
-            secrets ??= await Load<Dictionary<string, JsonData>>(nameof(JsonManager));
+
             Log.Info(DebugOption.Json, $"读取解密 => {obj.name.Orange()} 数据文件");
             secrets.TryAdd(obj.name, new JsonData());
             if (!secrets[obj.name]) return;
@@ -56,12 +57,13 @@ namespace JFramework.Core
         public static async Task<T> Decrypt<T>(string name) where T : new()
         {
             var filePath = GetPath(name);
+            secrets ??= await Load<Dictionary<string, JsonData>>(nameof(JsonManager));
             if (!File.Exists(filePath))
             {
                 Debug.Log($"{nameof(JsonManager)} 创建 => {name.Orange()} 数据文件");
                 await Encrypt(new T(), name);
             }
-            secrets ??= await Load<Dictionary<string, JsonData>>(nameof(JsonManager));
+
             Log.Info(DebugOption.Json, $"读取解密 => {name.Orange()} 数据文件");
             secrets.TryAdd(name, new JsonData());
             if (!secrets[name]) return default;
