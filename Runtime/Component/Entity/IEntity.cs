4deb8ac18d96f9a58fd3ec1dd44ca9119f63dcab
Charlotte <1176892094@qq.com>
2023-08-21 04:20:55 +0800

    优化

--- Runtime/Component/Entity/IEntity.cs
+++ Runtime/Component/Entity/IEntity.cs
@@ -1,7 +1,5 @@
-using System.Reflection;
 using JFramework.Core;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace JFramework.Interface
 {
@@ -54,88 +52,5 @@ namespace JFramework.Interface
     /// </summary>
     public interface IInject : IEntity
     {
-        /// <summary>
-        /// 获取子物体并注入字段
-        /// </summary>
-        /// <param name="obj"></param>
-        void Inject<T>(T obj) where T : IInject
-        {
-            var type = obj.GetType();
-            var fields = type.GetFields(Reflection.Instance);
-
-            foreach (var field in fields)
-            {
-                var inject = field.GetCustomAttribute<InjectAttribute>(true);
-                if (inject == null) continue;
-
-                var target = GetChild(obj.transform, inject.name);
-                if (target == null) continue;
-
-                var component = target.GetComponent(field.FieldType);
-                if (component != null)
-                {
-                    field.SetValue(obj, component);
-                }
-
-                var method = type.GetMethod(inject.name, Reflection.Instance);
-                if (method == null) continue;
-                if (target.TryGetComponent(out Button button) && component == button)
-                {
-                    if (obj.transform.TryGetComponent(out IPanel panel))
-                    {
-                        button.onClick.AddListener(() =>
-                        {
-                            if (panel.state == UIState.Freeze) return;
-                            obj.transform.SendMessage(inject.name);
-                        });
-                    }
-                    else
-                    {
-                        button.onClick.AddListener(() => obj.transform.SendMessage(inject.name));
-                    }
-                }
-                else if (target.TryGetComponent(out Toggle toggle) && component == toggle)
-                {
-                    if (obj.transform.TryGetComponent(out IPanel panel))
-                    {
-                        toggle.onValueChanged.AddListener(value =>
-                        {
-                            if (panel.state == UIState.Freeze) return;
-                            obj.transform.SendMessage(inject.name, value);
-                        });
-                    }
-                    else
-                    {
-                        toggle.onValueChanged.AddListener(value => obj.transform.SendMessage(inject.name, value));
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// 迭代查找子物体
-        /// </summary>
-        /// <param name="parent"></param>
-        /// <param name="name"></param>
-        /// <returns></returns>
-        Transform GetChild(Transform parent, string name)
-        {
-            for (int i = 0; i < parent.childCount; i++)
-            {
-                var child = parent.GetChild(i);
-                if (child.name == name)
-                {
-                    return child;
-                }
-
-                var result = GetChild(child, name);
-                if (result != null)
-                {
-                    return result;
-                }
-            }
-
-            return null;
-        }
     }
 }
--- Runtime/Extensions/InjectExtensions.cs
+++ Runtime/Extensions/InjectExtensions.cs
@@ -1,4 +1,7 @@
+using System.Reflection;
 using JFramework.Interface;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace JFramework
 {
@@ -13,7 +16,104 @@ namespace JFramework
         /// <param name="inject"></param>
         public static void Inject(this IInject inject)
         {
-            inject.Inject(inject);
+            var type = inject.GetType();
+            var fields = type.GetFields(Reflection.Instance);
+            foreach (var field in fields)
+            {
+                var attribute = field.GetCustomAttribute<InjectAttribute>(true);
+                if (attribute == null) continue;
+
+                var target = inject.transform.GetChild(attribute.name);
+                if (target == null) continue;
+
+                var component = target.GetComponent(field.FieldType);
+                if (component != null)
+                {
+                    field.SetValue(inject, component);
+                }
+
+                var method = type.GetMethod(attribute.name, Reflection.Instance);
+                if (method == null) continue;
+
+                if (target.TryGetComponent(out Button button) && component == button)
+                {
+                    inject.SetButton(attribute.name, button);
+                }
+                else if (target.TryGetComponent(out Toggle toggle) && component == toggle)
+                {
+                    inject.SetToggle(attribute.name, toggle);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 注册按钮
+        /// </summary>
+        /// <param name="inject"></param>
+        /// <param name="name"></param>
+        /// <param name="button"></param>
+        private static void SetButton(this IInject inject, string name, Button button)
+        {
+            if (inject.transform.TryGetComponent(out IPanel panel))
+            {
+                button.onClick.AddListener(() =>
+                {
+                    if (panel.state == UIState.Freeze) return;
+                    inject.transform.SendMessage(name);
+                });
+            }
+            else
+            {
+                button.onClick.AddListener(() => inject.transform.SendMessage(name));
+            }
+        }
+
+        /// <summary>
+        /// 注册开关
+        /// </summary>
+        /// <param name="inject"></param>
+        /// <param name="name"></param>
+        /// <param name="toggle"></param>
+        private static void SetToggle(this IInject inject, string name, Toggle toggle)
+        {
+            if (inject.transform.TryGetComponent(out IPanel panel))
+            {
+                toggle.onValueChanged.AddListener(value =>
+                {
+                    if (panel.state == UIState.Freeze) return;
+                    inject.transform.SendMessage(name, value);
+                });
+            }
+            else
+            {
+                toggle.onValueChanged.AddListener(value => inject.transform.SendMessage(name, value));
+            }
+        }
+
+        /// <summary>
+        /// 迭代查找子物体
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        private static Transform GetChild(this Transform parent, string name)
+        {
+            for (int i = 0; i < parent.childCount; i++)
+            {
+                var child = parent.GetChild(i);
+                if (child.name == name)
+                {
+                    return child;
+                }
+
+                var result = GetChild(child, name);
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+
+            return null;
         }
     }
 }
