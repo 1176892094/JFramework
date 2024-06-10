fe8c21a84f981228a6862484164f2adefcda21e0
Charlotte <1176892094@qq.com>
2024-06-11 00:05:45 +0800

    Update EditorSetting.cs

--- Runtime/Framework/EditorKit/EditorSetting.cs
+++ Runtime/Framework/EditorKit/EditorSetting.cs
@@ -107,7 +107,7 @@ namespace JFramework
             foreach (var folder in folders)
             {
                 if (string.IsNullOrEmpty(folder)) continue;
-                var name = Path.GetFileNameWithoutExtension(folder).ToLower();
+                var name = Path.GetFileNameWithoutExtension(folder);
                 var guids = AssetDatabase.FindAssets("t:Object", new[] { folder });
 
                 foreach (var guid in guids)
@@ -118,7 +118,7 @@ namespace JFramework
                         var importer = AssetImporter.GetAtPath(path);
                         if (importer == null) continue;
                         
-                        if (importer.assetBundleName != name)
+                        if (importer.assetBundleName != name.ToLower())
                         {
                             Debug.Log($"增加 AssetBundles 资源: {path.Green()}");
                             importer.assetBundleName = name;
