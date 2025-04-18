2ed6247d2388f0cdd659edbdf67d1f541810faeb
Molth Nevin <qq2201591870@163.com>
2025-04-18 16:53:31 +0800

    添加.net的random

---
+++ Assets/Astraia/Runtime/01.基础库/01.随机/BitOperationsHelpers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Astraia
+{
+    public static partial class Service
+    {
+        public static partial class Random
+        {
+            private static class BitOperationsHelpers
+            {
+                /// <summary>Rotates the specified value left by the specified number of bits.</summary>
+                /// <param name="value">The value to rotate.</param>
+                /// <param name="offset">
+                ///     The number of bits to rotate by. Any value outside the range [0..31] is treated as congruent mod
+                ///     32.
+                /// </param>
+                /// <returns>The rotated value.</returns>
+                [CLSCompliant(false)]
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                public static uint RotateLeft(uint value, int offset)
+                {
+                    return value << offset | value >> 32 - offset;
+                }
+
+                /// <summary>Rotates the specified value left by the specified number of bits.</summary>
+                /// <param name="value">The value to rotate.</param>
+                /// <param name="offset">
+                ///     The number of bits to rotate by. Any value outside the range [0..63] is treated as congruent mod
+                ///     64.
+                /// </param>
+                /// <returns>The rotated value.</returns>
+                [CLSCompliant(false)]
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                public static ulong RotateLeft(ulong value, int offset)
+                {
+                    return value << offset | value >> 64 - offset;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Astraia/Runtime/01.基础库/01.随机/MathHelpers.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Astraia
+{
+    public static partial class Service
+    {
+        public static partial class Random
+        {
+            private static class MathHelpers
+            {
+                /// <summary>Produces the full product of two unsigned 64-bit numbers.</summary>
+                /// <param name="a">The first number to multiply.</param>
+                /// <param name="b">The second number to multiply.</param>
+                /// <param name="low">The low 64-bit of the product of the specified numbers.</param>
+                /// <returns>The high 64-bit of the product of the specified numbers.</returns>
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                public static ulong BigMul(ulong a, ulong b, out ulong low)
+                {
+                    uint al = (uint)a;
+                    uint ah = (uint)(a >> 32);
+                    uint bl = (uint)b;
+                    uint bh = (uint)(b >> 32);
+                    ulong mull = (ulong)al * bl;
+                    ulong t = (ulong)ah * bl + (mull >> 32);
+                    ulong tl = (ulong)al * bh + (uint)t;
+                    low = (tl << 32) | (uint)mull;
+                    return (ulong)ah * bh + (t >> 32) + (tl >> 32);
+                }
+            }
+        }
+    }
+}
